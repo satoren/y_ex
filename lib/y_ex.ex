e44e2fcdbf3c139837fe973881c4533aa1033e75
satoren <satoreyo@hotmail.com>
2024-07-20 11:34:58 +0900

    define type spec

--- lib/y_ex.ex
+++ lib/y_ex.ex
@@ -11,8 +11,17 @@ defmodule Yex do
       iex> Yex.encode_state_vector(doc)
       {:ok, <<0>>}
   """
+  @spec encode_state_vector(Yex.Doc.t()) :: {:ok, binary()} | {:error, term()}
   def encode_state_vector(%Yex.Doc{} = doc) do
     Yex.Nif.encode_state_vector(doc)
+  end
+
+  @spec encode_state_vector!(Yex.Doc.t()) :: {:ok, binary()} | {:error, term()}
+  def encode_state_vector!(%Yex.Doc{} = doc) do
+    case encode_state_vector(doc) do
+      {:ok, binary} -> binary
+      {:error, reason} -> raise reason
+    end
   end
 
   @doc """
@@ -23,8 +32,17 @@ defmodule Yex do
       iex> Yex.encode_state_as_update(doc)
       {:ok, <<0, 0>>}
   """
+  @spec encode_state_as_update(Yex.Doc.t(), binary()) :: {:ok, binary()} | {:error, term()}
   def encode_state_as_update(%Yex.Doc{} = doc, encoded_state_vector \\ nil) do
     Yex.Nif.encode_state_as_update(doc, encoded_state_vector)
+  end
+
+  @spec encode_state_as_update!(Yex.Doc.t(), binary()) :: binary()
+  def encode_state_as_update!(%Yex.Doc{} = doc, encoded_state_vector \\ nil) do
+    case encode_state_as_update(doc, encoded_state_vector) do
+      {:ok, binary} -> binary
+      {:error, reason} -> raise reason
+    end
   end
 
   @doc """
@@ -40,6 +58,7 @@ defmodule Yex do
       iex> Yex.apply_update(doc2, state1)
       :ok
   """
+  @spec apply_update(Yex.Doc.t(), binary()) :: :ok
   def apply_update(%Yex.Doc{} = doc, update) do
     Yex.Nif.apply_update(doc, update) |> unwrap_ok_tuple()
   end
