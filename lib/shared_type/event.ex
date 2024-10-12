38171db5942550cd254a97bb1481e49a2c3900a9
satoren <satoreyo@hotmail.com>
2024-10-12 10:14:32 +0900

    wip

--- lib/shared_type/event.ex
+++ lib/shared_type/event.ex
@@ -1,6 +1,10 @@
 defmodule Yex.ArrayEvent do
   @moduledoc """
+  Event when Array type changes
 
+  @see Yex.Array.observe/1
+  @see Yex.Array.observe_deep/1
+  @see Yex.Map.observe_deep/1
   """
   defstruct [
     :path,
@@ -16,6 +20,11 @@ end
 defmodule Yex.MapEvent do
   @moduledoc """
 
+  Event when Map type changes
+
+  @see Yex.Map.observe/1
+  @see Yex.Array.observe_deep/1
+  @see Yex.Map.observe_deep/1
   """
   defstruct [
     :path,
