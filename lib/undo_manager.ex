36fe98f6271deaaf894e3d65d488b15964dd461d
Cort Fritz <cort@rheli.co>
2024-11-16 19:36:51 -0800

    save WiP of undo with origin

--- lib/undo_manager.ex
+++ lib/undo_manager.ex
@@ -3,4 +3,25 @@ defmodule Yex.UndoManager do
   Represents a Y.UndoManager instance.
   """
   defstruct [:reference]
+
+  @doc """
+  Creates a new UndoManager for the given document.
+  """
+  def new(doc) do
+    Yex.Nif.undo_manager_new(doc)
+  end
+
+  @doc """
+  Includes an origin to be tracked by the UndoManager.
+  """
+  def include_origin(undo_manager, origin) do
+    Yex.Nif.undo_manager_include_origin(undo_manager, origin)
+  end
+
+  @doc """
+  Undoes the last tracked change.
+  """
+  def undo(undo_manager) do
+    Yex.Nif.undo_manager_undo(undo_manager)
+  end
 end
--- native/yex/src/undo.rs
+++ native/yex/src/undo.rs
@@ -20,8 +20,18 @@ pub fn undo_manager_new(doc: NifDoc) -> NifUndoManager {
     let undo_manager = UndoManager::with_options(&doc.reference.doc, Options::default());
     
     let resource = UndoManagerResource::from(RwLock::new(undo_manager));
+    let arc = ResourceArc::new(resource);
+    
+    let arc_clone = arc.clone();
+    let _ = doc.reference.doc.observe_update_v2(move |_txn, _e| {
+        if let Ok(_manager) = arc_clone.0.write() {
+            // The UndoManager will track changes automatically
+            // when they're made with the correct origin
+        }
+    });
+    
     NifUndoManager {
-        reference: ResourceArc::new(resource),
+        reference: arc,
     }
 }
 
@@ -32,6 +42,18 @@ pub fn undo_manager_include_origin(undo_manager: NifUndoManager, origin: Binary)
 }
 
 #[rustler::nif]
-pub fn undo_manager_undo(undo_manager: NifUndoManager) {
-    undo_manager.reference.0.write().unwrap().undo();
+pub fn undo_manager_undo(undo_manager: NifUndoManager) -> bool {
+    let mut manager = undo_manager.reference.0.write().unwrap();
+    match manager.try_undo() {
+        Ok(did_undo) => {
+            println!("Undo result: {}", did_undo);
+            did_undo
+        },
+        Err(e) => {
+            println!("Undo error: {:?}", e);
+            false
+        }
+    }
 }
+
+
